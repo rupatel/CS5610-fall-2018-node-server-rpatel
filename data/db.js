aaeb07395d802f150b4ef46537a9c3713bf2bef8
rupatel <patel.ru@husky.neu.edu>
2018-11-28 17:01:15 -0500

    update mongo url

--- data/db.js
+++ data/db.js
@@ -1,7 +1,8 @@
 module.exports = function () {
     const mongoose = require('mongoose');
     const databaseName = 'whiteboard';
-    var connectionStr = 'mongodb://localhost/';
-    connectionStr += databaseName;
+    // var connectionStr = 'mongodb://localhost/';
+    // connectionStr += databaseName;
+    var connectionStr = process.env.MONGODB_URI;
     mongoose.connect(connectionStr)
 };
