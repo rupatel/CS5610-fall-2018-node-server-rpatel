2e6542d6033e8738145ff927eca7cfa39425cae4
rupatel <patel.ru@husky.neu.edu>
2018-11-28 17:26:58 -0500

    url mongos

--- data/db.js
+++ data/db.js
@@ -4,5 +4,6 @@ module.exports = function () {
     // var connectionStr = 'mongodb://localhost/';
     // connectionStr += databaseName;
     var connectionStr = process.env.MONGODB_URI;
-    mongoose.connect(connectionStr)
-};
+    console.log(connectionStr+'<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+    mongoose.connect('mongodb://heroku_ml4vk7x3:i9pge9lp9509lg7epcv0mmd9oi@ds129966.mlab.com:29966/heroku_ml4vk7x3');
+}
