f4ccc14ce004f66e3e5ad309112421eff6ce79f7
rupatel <patel.ru@husky.neu.edu>
2018-11-28 17:42:10 -0500

    heroku deployment

--- data/db.js
+++ data/db.js
@@ -4,6 +4,5 @@ module.exports = function () {
     // var connectionStr = 'mongodb://localhost/';
     // connectionStr += databaseName;
     var connectionStr = process.env.MONGODB_URI;
-    console.log(connectionStr+'<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-    mongoose.connect('mongodb://heroku_ml4vk7x3:i9pge9lp9509lg7epcv0mmd9oi@ds129966.mlab.com:29966/heroku_ml4vk7x3');
+    mongoose.connect(connectionStr);
 }
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 const bodyParser = require('body-parser');
-const universityDao = require('./daos/university.dao.server');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,4 +10,4 @@ const questionService = require('./services/question.service.server')(app);
 const answerService = require('./services/answer.service.server')(app);
 const studentService = require('./services/student.service.server')(app);
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
